50b8c669e8378b9c6b0944f08a88325d85288dd5
Hemang <hemang2050@gmail.com>
2021-06-15 20:40:04 +0530

    first

---
+++ src/constants/constants.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: 'Plasma Life',
+    description: "A plasma donation app that helps create a network of plasma donors and covid patients.",
+      image: '/images/1.png',
+      tags: ['Bootstrap', 'React', 'Firebase'],
+    source: 'https://jodhpur-plasma.netlify.app/',
+    visit: 'https://github.com/hemang7/Plasma_Donation',
+    id: 0,
+  },
+  {
+    title: 'Photo Gallery',
+    description:"A photo gallery app that lets you store and view your precious moments.",
+    image: '/images/2.png',
+    tags: ['React', 'JavaScript', 'Firebase'],
+    source: 'https://hm-photo-gallery.netlify.app/',
+    visit: 'https://github.com/hemang7/photo-gallery',
+    id: 1,
+  },
+  {
+    title: 'Todo App',
+    description: "A simple to-do app that lets you maintain and delete your notes.",
+      image: '/images/3.png',
+      tags: ['HTML', 'CSS','React','Javascript'],
+    source: 'https://todo-hm.herokuapp.com//',
+    visit: 'https://github.com/hemang7/todo-app',
+    id: 2,
+  },
+  {
+    title: 'IPL Rooster',
+    description: "An IPL rooster app that keeps a track of all team squads.",
+    image: '/images/4.png',
+    tags: ['React', 'Javascript'],
+    source: 'https://ipl-rooster.netlify.app/',
+    visit: 'https://github.com/hemang7/ipl',
+    id: 3,
+  },
+];
+
+
+export const blogs = [
+  {
+    title: 'How to add an existing project to your Github repo.',
+    source: 'https://hm-blog.hashnode.dev/how-to-add-existing-project-to-a-github-repository',
+    id: 0,
+  }
+
+]
